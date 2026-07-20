b793562bb9b3087525b0404be1346e3cc4a5de11
Mohammad Wasiq Khan <mohdwasiqkhan123@gmail.com>
2026-07-21 04:17:52 +0530

    Added vw_scenario_analysis_dataset

--- sql/08_Create_Dashboard_Views.sql
+++ sql/08_Create_Dashboard_Views.sql
@@ -13,14 +13,14 @@ WITH
 			reporter_iso3,
             reporter_name,
             reference_year,
-            SUM(trade_value_usd) AS total_exports_usd,
-			SUM(fob_value_usd) AS total_fob_exports_usd,
-            MAX(gdp_current_usd) AS gdp_current_usd,
-			MAX(population_total) AS population_total,
-			MAX(gdp_growth_annual_pct) AS gdp_growth_annual_pct,
-			MAX(trade_pct_of_gdp) AS trade_pct_of_gdp,
-            COUNT(DISTINCT partner_iso3) AS total_trade_partners,
-			COUNT(DISTINCT commodity_code) AS total_commodities
+            SUM(trade_value_usd) 											AS total_exports_usd,
+			SUM(fob_value_usd) 												AS total_fob_exports_usd,
+            MAX(gdp_current_usd) 											AS gdp_current_usd,
+			MAX(population_total) 											AS population_total,
+			MAX(gdp_growth_annual_pct) 										AS gdp_growth_annual_pct,
+			MAX(trade_pct_of_gdp) 											AS trade_pct_of_gdp,
+            COUNT(DISTINCT partner_iso3) 									AS total_trade_partners,
+			COUNT(DISTINCT commodity_code) 									AS total_commodities
 		FROM 
 			fact_global_trade_intelligence_dataset
 		GROUP BY
@@ -40,7 +40,7 @@ WITH
 		trade_pct_of_gdp,
         total_trade_partners,
 		total_commodities,
-        RANK() OVER(PARTITION BY reference_year ORDER BY total_exports_usd DESC ) AS global_export_rank
+        RANK() OVER(PARTITION BY reference_year ORDER BY total_exports_usd DESC ) 	AS global_export_rank
 	FROM 
     country_summary;
 
@@ -58,11 +58,11 @@ WITH
 			reporter_iso3,
 			reporter_name,
 			reference_year,
-            SUM(trade_value_usd) AS total_exports_usd,
-            MAX(gdp_current_usd) AS gdp_current_usd,
-			MAX(population_total) AS population_total,
-			MAX(gdp_growth_annual_pct) AS gdp_growth_annual_pct,
-			MAX(trade_pct_of_gdp) AS trade_pct_of_gdp
+            SUM(trade_value_usd) 													AS total_exports_usd,
+            MAX(gdp_current_usd) 													AS gdp_current_usd,
+			MAX(population_total) 													AS population_total,
+			MAX(gdp_growth_annual_pct) 												AS gdp_growth_annual_pct,
+			MAX(trade_pct_of_gdp) 													AS trade_pct_of_gdp
 		FROM 
 			fact_global_trade_intelligence_dataset
 		GROUP BY
@@ -79,9 +79,59 @@ WITH
 			population_total,
 			gdp_growth_annual_pct,
 			trade_pct_of_gdp,
-            RANK() OVER(PARTITION BY reference_year ORDER BY total_exports_usd DESC) AS global_export_rank,
-            LAG(total_exports_usd) OVER(PARTITION BY reporter_iso3 ORDER BY reference_year) AS previous_year_exports,
-            ROUND((total_exports_usd - LAG(total_exports_usd) OVER(PARTITION BY reporter_iso3 ORDER BY reference_year)) / 
-				NULLIF(LAG(total_exports_usd) OVER(PARTITION BY reporter_iso3 ORDER BY reference_year), 0 ) * 100, 2 ) AS yoy_export_growth_pct
+            RANK() OVER(PARTITION BY reference_year ORDER BY total_exports_usd DESC) 						AS global_export_rank,
+            LAG(total_exports_usd) OVER(PARTITION BY reporter_iso3 ORDER BY reference_year) 				AS previous_year_exports,
+            ROUND((total_exports_usd - LAG(total_exports_usd) OVER
+				(PARTITION BY reporter_iso3 ORDER BY reference_year)) / NULLIF(LAG(total_exports_usd) OVER
+					(PARTITION BY reporter_iso3 ORDER BY reference_year), 0 ) * 100, 2 ) 					AS yoy_export_growth_pct
 		FROM 
 			dashboard_data;
+            
+
+-- NOTE: Below View added later on while creating MS Excel workbook.
+-- Creating view: vw_scenario_analysis_dataset
+-- This has been created for using this dataset in MS Excel for creating Scenario Analysis dashboard. This will provide business metrics that will be used for What-If Analysis.
+DROP VIEW IF EXISTS vw_scenario_analysis_dataset;
+CREATE VIEW 
+	vw_scenario_analysis_dataset
+AS
+WITH 
+	scenario_base 
+	AS
+    (
+		SELECT
+			reporter_iso3,
+			reporter_name,
+			reference_year,
+            SUM(trade_value_usd) 									AS total_exports_usd,
+            MAX(gdp_current_usd) 									AS gdp_current_usd,
+            MAX(population_total) 									AS population_total,
+            MAX(gdp_growth_annual_pct) 								AS gdp_growth_annual_pct,
+            MAX(trade_pct_of_gdp) 									AS trade_pct_of_gdp
+		FROM 
+			fact_global_trade_intelligence_dataset
+		GROUP BY
+			reporter_iso3,
+            reporter_name,
+            reference_year
+	)
+		SELECT
+			reporter_iso3,
+            reporter_name,
+            reference_year,
+            total_exports_usd,
+            gdp_current_usd,
+            population_total,
+            gdp_growth_annual_pct,
+            trade_pct_of_gdp,
+            ROUND( total_exports_usd / NULLIF(population_total,0), 2 ) 								AS export_per_capita,
+            ROUND( gdp_current_usd / NULLIF(population_total,0), 2 ) 								AS gdp_per_capita,
+            ROUND( (total_exports_usd / NULLIF(gdp_current_usd,0))*100, 2 ) 						AS export_to_gdp_ratio_pct,
+            RANK() OVER ( PARTITION BY reference_year ORDER BY total_exports_usd DESC ) 			AS global_export_rank,
+            LAG(total_exports_usd) OVER ( PARTITION BY reporter_iso3 ORDER BY reference_year ) 		AS previous_year_exports,
+            ROUND( (total_exports_usd - LAG(total_exports_usd) OVER 
+				( PARTITION BY reporter_iso3 ORDER BY reference_year)) / 
+					NULLIF(LAG(total_exports_usd) OVER (
+						PARTITION BY reporter_iso3 ORDER BY reference_year), 0)*100, 2) 			AS yoy_export_growth_pct
+		FROM 
+			scenario_base;
